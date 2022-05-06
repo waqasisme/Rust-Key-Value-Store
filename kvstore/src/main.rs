212d6c04782d32cfb758da3d1d05fc23e38eb3ea
Muhammad Waqas <muhammad.waqas@rwth-aachen.de>
2022-05-06 14:19:20 +0200

    Finished key value store

--- kvstore/src/main.rs
+++ kvstore/src/main.rs
@@ -1,16 +1,15 @@
-use std::{collections::HashMap, fs::read_to_string};
+use std::collections::HashMap;
 
 fn main() {
     let mut arguments = std::env::args().skip(1);
     let key = arguments.next().expect("Key was not there");
     let value = arguments.next().expect("value missing");
     println!("The key is '{}', the value is '{}'", key, value);
-
-    let contents = format!("{}\t{}\n", key, value);
     // empty tuple = unit (similar to void)
 
-    let database = Database::new().expect("Database::new() crashed");
-    
+    let mut database = Database::new().expect("Database::new() crashed");
+    database.insert(key.to_uppercase(), value.clone());
+    database.insert(key, value);
 }
 
 struct Database {
@@ -20,14 +19,11 @@ struct Database {
 impl Database {
     fn new() -> Result<Database, std::io::Error> {
         // read kv.db, parse and populate map
-        // let contents = match std::fs::read_to_string("kv.db") {
-        //     Ok(c) => c>,
-        //     Err(e) => {
-        //         return Err(e);
-        //     }
-        // };
         let mut map = HashMap::new();
-        let contents = std::fs::read_to_string("kv.db")?;
+        if !std::path::Path::new("kvstore.db").exists() {
+            std::fs::write("kvstore.db", "")?;
+        }
+        let contents = std::fs::read_to_string("kvstore.db")?;
         for line in contents.lines() {
             let (key, value) = line.split_once('\t').expect("Corrupt Database!");
             map.insert(key.to_owned(), value.to_owned());
@@ -35,4 +31,24 @@ impl Database {
 
         Ok(Database { map })
     }
+
+    fn insert(&mut self, key: String, value: String) {
+        self.map.insert(key, value);
+    }
+
+    fn flush(&self) -> std::io::Result<()> {
+        // not taking self by reference so that flush is the last thing called
+        let mut contents = String::new();
+        for (key, value) in &self.map {
+            contents.push_str(&format!("{}\t{}\n", key, value));
+        }
+        std::fs::write("kvstore.db", contents)
+    }
 }
+
+impl Drop for Database {
+    fn drop(&mut self) {
+        let _ = self.flush();
+    }
+}
+
